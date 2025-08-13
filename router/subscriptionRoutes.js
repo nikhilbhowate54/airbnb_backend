f5991b252e201c2e0e4fd1f874c5c041d0b3022c
stellerNikhil <nikhil@stellarflux.com>
2025-08-13 12:57:02 +0530

    fixed bug in subscription

--- router/subscriptionRoutes.js
+++ router/subscriptionRoutes.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const router = express.Router();
 const SubscriptionPlan = require("../models/SubscriptionPlan");
-const User = require("../models/User");
+const User = require("../models/users");
 const auth = require("../middleware/auth");
 
 // Host views all available plans
